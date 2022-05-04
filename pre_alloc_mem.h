6c081a0b530b11e9283190bdb5f8122ecc1c60e4
wezelis2 <wezelis2@illinois.edu>
2022-05-03 20:07:04 -0500

    add comment

--- pre_alloc_mem.h
+++ pre_alloc_mem.h
@@ -10,10 +10,10 @@ int get_pre_alloc_mem(long num_bytes, char **ptr);
 int free_pre_alloc_mem(char *ptr);
 int free_all_pre_alloc_mem();
 
-//some simple functions used only for testing
-int test_pre_alloc_internal_check();
-long test_pre_alloc_get_free();
-long test_pre_alloc_get_used();
-int test_print_LL();
+//some simple functions used only for testing (uncomment to run test program)
+// int test_pre_alloc_internal_check();
+// long test_pre_alloc_get_free();
+// long test_pre_alloc_get_used();
+// int test_print_LL();
 
 #endif //PRE_ALLOC_MEM
